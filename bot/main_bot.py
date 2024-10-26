571b1263c64930da967573c743fa8cba94ff9aa5
mike <simenehmikiyas977@gmail.com>
2024-10-26 15:34:04 -0400

    change status

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -46,6 +46,9 @@ class MainBot(Bot):
 
     async def on_ready(self):
         print(f'Logged in as {self.user.name} ({self.user.id})')
+        # Setting a custom status
+        activity = discord.Game("helping with everything 🐧")
+        await self.change_presence(status=discord.Status.online, activity=activity)
         await self.tree.sync() 
         
 
